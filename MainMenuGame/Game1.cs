2b5b641318963535b73ed8010b60651e67f233f6
afreshnock <picklefish01@gmail.com>
2023-09-01 17:52:49 -0500

    Created Menu for game

--- MainMenuGame/Game1.cs
+++ MainMenuGame/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using SharpDX.Direct3D9;
+using System.Drawing.Printing;
 
 namespace MainMenuGame
 {
@@ -8,6 +11,10 @@ namespace MainMenuGame
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Texture2D plant;
+        private MouthGuy mouthGuy;
+        private SpriteFont font;
+
 
         public Game1()
         {
@@ -19,33 +26,43 @@ namespace MainMenuGame
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            mouthGuy = new MouthGuy() { Position = new Vector2(_graphics.GraphicsDevice.Viewport.Width /2 , _graphics.GraphicsDevice.Viewport.Height / 2) };
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            mouthGuy.LoadContent(Content);
+            plant = Content.Load<Texture2D>("Bush");
+            font = Content.Load<SpriteFont>("File");
             // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (mouthGuy.exit)
                 Exit();
 
             // TODO: Add your update logic here
-
+            mouthGuy.Update(gameTime);
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            GraphicsDevice.Clear(Color.SandyBrown);
+            _spriteBatch.Begin();
             // TODO: Add your drawing code here
+            _spriteBatch.Draw(plant, new Vector2(100, 100), Color.White);
+            _spriteBatch.Draw(plant, new Vector2(210, 300), Color.White);
+            _spriteBatch.Draw(plant, new Vector2(400, 40), Color.White);
+            _spriteBatch.Draw(plant, new Vector2(500, 400), Color.White);
+            _spriteBatch.Draw(plant, new Vector2(60, 450), Color.White);
+            _spriteBatch.DrawString(font, "Hold Space to Exit Game", new Vector2(250, 100), Color.Black);
+            mouthGuy.Draw(gameTime, _spriteBatch);
 
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
---
+++ MainMenuGame/MouthGuy.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Drawing.Printing;
+using Microsoft.Xna.Framework.Content;
+using SharpDX.Direct2D1.Effects;
+using SharpDX.DXGI;
+
+namespace MainMenuGame
+{
+    public class MouthGuy
+    {
+        public Vector2 Position;
+        private short animationFrame;
+        private Texture2D texture;
+        private KeyboardState prior;
+        private KeyboardState current;
+        private double animationTimer;
+        private float scaling = 5;
+        public bool exit;
+        private bool held;
+
+        public void LoadContent(ContentManager contentManager)
+        {
+            texture = contentManager.Load<Texture2D>("mouth guy");
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            prior = current;
+            current = Keyboard.GetState();
+            if(current.IsKeyDown(Keys.Space) && prior.IsKeyDown(Keys.Space))
+            {
+                if(held == false) 
+                {
+                    held = true;
+                    animationFrame = 0;
+                }
+                
+                animationTimer += gameTime.ElapsedGameTime.TotalSeconds;
+                if (animationTimer > 0.1)
+                {
+                    animationFrame++;
+                    if (animationFrame == 3)
+                    {
+                        animationFrame = 2;
+                        scaling++;
+                        if (scaling >= 25) exit = true;
+                    }
+                    animationTimer -= 0.1;
+                }
+            }
+            else
+            {
+                if (held)
+                {
+                    held = false;
+                    animationFrame = 0;
+                    scaling = 5;
+                }
+                else
+                {
+                    animationTimer += gameTime.ElapsedGameTime.TotalSeconds;
+                    if (animationTimer > 1)
+                    {
+                        if (animationFrame == 0 || animationFrame == 4)
+                        {
+                            animationFrame = 3;
+                        }
+                        else if (animationFrame == 3)
+                        {
+                            animationFrame = 4;
+                        }
+                        
+                        animationTimer -= 1;
+                    }
+                }
+            }
+
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            Rectangle source = new Rectangle(0 , animationFrame * 32 , 32 ,32);
+            spriteBatch.Draw(texture, Position, source, Color.White, 0, new Vector2(16f, 16f), scaling, SpriteEffects.None, 0);
+        }
+
+    }
+}
